00f736891c83b48d2e2de977d8e6f63a47b44924
Krishjampana <123447546+Krishjampana@users.noreply.github.com>
2024-12-14 08:17:42 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
-    $con = mysqli_connect("127.0.0.1","root","","easyrentals");
+   // $con = mysqli_connect("127.0.0.1","root","","easyrentals");
+$con = mysqli_connect("i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "un5pp9hupo6705pc", "oo8cd6urgrmvh4ot", "bcn698x5ryjdilel");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
